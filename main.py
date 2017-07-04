7ca62e8e4a95fd9e2aac4921b0a423d59ac16a55
Ajay Guleria <ajg.public@yahoo.com>
2017-07-03 18:38:34 -0700

    Add argument parsing so we can pass script name to run

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 from flask import Flask, render_template, request, Response
 from subprocess import Popen, PIPE
+from argparse import ArgumentParser
+import sys
+
 app = Flask(__name__)
+
+class Args(object):
+  script = 'test.sh'
+
+args = Args()
 
 @app.route("/")
 def hello():
-	return render_template('first.html')
+  return render_template('first.html')
 
 @app.route("/runInitScripts")
 def runScripts():
   dirpath  = request.values.get("dir")
-  print(dirpath)
+  #print(dirpath)
   def inner():
-    cmd = ["scripts/test.sh"] 
-    proc = Popen(cmd,stdout=PIPE,stderr=PIPE,stdin=PIPE)
-    for line in iter(proc.stdout.readline,''):
-      yield line.rstrip() + b'<br/>\n'
+    cmd = ['scripts/' + args.script, dirpath] 
+    try:
+      proc = Popen(cmd,stdout=PIPE,stderr=PIPE,stdin=PIPE)
+      for line in iter(proc.stdout.readline,''):
+        yield line.rstrip() + b'<br/>\n'
+    except BaseException as e:
+      yield str(e) + '<br/>\n' 
 	
   return Response(inner(), mimetype='text/html')	
         
@@ -23,18 +34,22 @@ def runScripts():
 def train():
   def inner():
     proc = Popen(
-        ["processes/train.py"],             #call something with a lot of output so we can see it
+        ["processes/train.py"],   #call something with a lot of output so we can see it
         shell=True,
         stdout=PIPE
     )
 
     for line in iter(proc.stdout.readline,''):
-        yield line.rstrip() + '<br/>\n'
+        yield line.rstrip() + b'<br/>\n'
 
   return Response(inner(), mimetype='text/html')  # text/html is required for most browsers to show the page
 
 
 if __name__ == "__main__":
+  parser = ArgumentParser(description='Run a web server that starts local scripts')
+  parser.add_argument('--script', '-s', default='test.sh', help='Script to run')
+  parser.parse_args(sys.argv[1:], args)
+
   try:
     app.run(host='0.0.0.0',port=8080)
   except KeyboardInterrupt:
