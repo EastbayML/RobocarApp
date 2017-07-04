f240db692e61d267bd3e6b66404033133a2e7860
Ajay Guleria <ajg.public@yahoo.com>
2017-07-03 19:20:03 -0700

    Add a part to remote copy files between servers

--- main.py
+++ main.py
@@ -14,6 +14,26 @@ args = Args()
 def hello():
   return render_template('first.html')
 
+@app.route("/raspberry")
+def raspberry():
+  return render_template('raspberry.html')
+
+@app.route("/runRPiScripts")
+def runRpiScripts():
+  from_dir  = request.values.get("from")
+  to_dir  = request.values.get("to")
+  def inner():
+    cmd = ['bash', '-c', 'rsync -razv -e ssh ' +  from_dir + ' ' + to_dir] 
+    try:
+      proc = Popen(cmd,stdout=PIPE,stderr=PIPE,stdin=PIPE)
+      for line in iter(proc.stdout.readline,''):
+        return line.rstrip() + b'<br/>\n'
+    except BaseException as e:
+      return str(e) + '<br/>\n' 
+	
+  return Response(inner(), mimetype='text/html')	
+        
+
 @app.route("/runInitScripts")
 def runScripts():
   dirpath  = request.values.get("dir")
@@ -33,14 +53,19 @@ def runScripts():
 @app.route("/train")
 def train():
   def inner():
-    proc = Popen(
+    try:
+      proc = Popen(
         ["processes/train.py"],   #call something with a lot of output so we can see it
         shell=True,
         stdout=PIPE
-    )
+      )
 
-    for line in iter(proc.stdout.readline,''):
+      for line in iter(proc.stdout.readline,''):
         yield line.rstrip() + b'<br/>\n'
+
+    except BaseException as e:
+      yield str(e) + '<br/>\n' 
+
 
   return Response(inner(), mimetype='text/html')  # text/html is required for most browsers to show the page
 
